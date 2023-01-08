bab00532748e6d27b58d8e3cb8233b18aea61e45
Raja Gopla R <109989163+Rajagopal9597@users.noreply.github.com>
2023-01-08 15:41:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ mongoose
     })
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+const PORT = process.env.PORT || 3000
 
 app.use('/',registerRoutes);
 app.use('/',signinRoutes);
